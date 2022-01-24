1d6356f6f84d16093069b3d7366165eac79eca36
Alessio Napoli <alessionapoli99@icloud.com>
2022-01-24 15:24:39 +0100

    Create all parts of HTML and CSS

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-// Inserire Età
+// Create Let
 
-// Inserire Km da percorrere
+let name = document.getElementById("name");
+let km = document.getElementById("km");
+let age = document.getElementById("age");
 
+// Create event button
+let button = document.getElementById("generate");
 
+button.addEventListener("click",
+  function(){
+    alert("My name is" + name.value + km.value + age.value)
+  }
+)
 // Stabilisco Prezzo fisso Km
 let prezzoKm = 0.21;
-
 
 // Calcolo Prezzo totale senza sconto
 let prezzoTotale = km * prezzoKm;
@@ -13,17 +21,13 @@ let prezzoTotale = km * prezzoKm;
 // Calcolo dello sconto
 let sale = 0;
 
-if(eta < 18){
+if(age < 18){
   sale = (prezzoTotale / 100) * 20;
-  document.getElementById("age").innerHTML = "Hai ottenuto lo sconto per i Minorenni del 20%";
-} else if(eta > 65){
+} else if(age > 65){
   sale = (prezzoTotale /100) * 40;
-  document.getElementById("age").innerHTML = "Hai ottenuto lo sconto per gli Over 65 del 40%";
 } else{
-  document.getElementById("age").innerHTML = "Non hai i requisiti per ottenere uno sconto."
+  sale = 0;
 }
 
 // Calcolo Prezzo Scontato
 let salePrice= prezzoTotale - sale;
-
-document.getElementById("price").innerHTML = "Il prezzo del biglietto è: " + salePrice.toFixed(2) + "€";
