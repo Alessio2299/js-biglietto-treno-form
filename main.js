d167e0b7d1a79a7688cab923aa2d21b72405b340
Alessio Napoli <alessionapoli99@icloud.com>
2022-01-24 18:16:05 +0100

    Add Event Button Cancel

--- main.js
+++ main.js
@@ -3,45 +3,48 @@
 let name = document.getElementById("name");
 let km = document.getElementById("km");
 let age = document.getElementById("age");
-let underAge = 17;
-let adult = 18;
-let over65 = 65;
 
-
-// Stabilisco Prezzo fisso Km
+// Fixed Price
 let prezzoKm = 0.21;
 
 // Create event button
-let button = document.getElementById("generate");
+let generate = document.getElementById("generate");
+let cancel = document.getElementById("cancel");
 let randomNumber = Math.floor( Math.random() * 9) + 1;
 let codeCp = Math.floor( Math.random() * (100000-90000+1)) + 90000;
 
-button.addEventListener("click",
+generate.addEventListener("click",
   function(){
-  // Calcolo Prezzo totale senza sconto
+    // Calculation Price without the sale
     let prezzoTotale = km.value * prezzoKm;
     let sale;
 
-  // Calcolo dello sconto
+    // Calculation Sale
 
-    if(underAge < 18){
+    if(age.value == "underAge"){
       sale = (prezzoTotale / 100) * 20;
-    } else if(over65 >= 65 ){
+    } else if(age.value == "over65"){
       sale = (prezzoTotale /100) * 40;
     } else{
       sale = 0;
     }
-    console.log(age.value)
-    // Calcolo Prezzo Scontato
-    let salePrice= prezzoTotale - sale;
 
+    // Calculation Price with sale
+    let salePrice= prezzoTotale - sale;
+    document.querySelector(".container-ticket").classList.remove("hidden");
     document.querySelector(".details-name").innerHTML += "<span>" + name.value + "</span>";
     document.querySelector(".carriage").innerHTML += "<span>" + randomNumber + "</span>";
     document.querySelector(".code-CP").innerHTML += "<span>" + codeCp + "</span>";
-    document.querySelector(".price-ticket").innerHTML += "<span>" + salePrice.toFixed(2) + "€" + "</span>";
-    
-  
-    
+    document.querySelector(".price-ticket").innerHTML += "<span>" + salePrice.toFixed(2) + "€" + "</span>"; 
+
   }
 )
 
+cancel.addEventListener("click",
+  function(){
+    document.querySelector(".container-ticket").classList.add("hidden");
+    name.value = "";
+    km.value = "";
+    age.value = ""; 
+  }
+)
