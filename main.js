0849430d1ca91f6640d835265a7fc1e267eaf828
Alessio Napoli <alessionapoli99@icloud.com>
2022-01-24 17:13:57 +0100

    Added with querySelector  Name and code into ticket

--- main.js
+++ main.js
@@ -3,36 +3,45 @@
 let name = document.getElementById("name");
 let km = document.getElementById("km");
 let age = document.getElementById("age");
+let underAge = 17;
+let adult = 18;
+let over65 = 65;
+
 
 // Stabilisco Prezzo fisso Km
 let prezzoKm = 0.21;
 
-// Calcolo Prezzo totale senza sconto
-let prezzoTotale = km.value * prezzoKm;
-console.log(prezzoTotale);
-
-// Calcolo dello sconto
-let sale;
-
-if(age.value < 18){
-  sale = (prezzoTotale / 100) * 20;
-} else if(age.value > 65){
-  sale = (prezzoTotale /100) * 40;
-} else{
-  sale = 0;
-}
-
-// Calcolo Prezzo Scontato
-let salePrice= prezzoTotale - sale;
-
 // Create event button
 let button = document.getElementById("generate");
-let randomNumber = Math.floor( Math.random() * 9);
-console.log(randomNumber);
+let randomNumber = Math.floor( Math.random() * 9) + 1;
+let codeCp = Math.floor( Math.random() * (100000-90000+1)) + 90000;
+
 button.addEventListener("click",
   function(){
+  // Calcolo Prezzo totale senza sconto
+    let prezzoTotale = km.value * prezzoKm;
+    let sale;
+
+  // Calcolo dello sconto
+
+    if(underAge < 18){
+      sale = (prezzoTotale / 100) * 20;
+    } else if(over65 >= 65 ){
+      sale = (prezzoTotale /100) * 40;
+    } else{
+      sale = 0;
+    }
+    console.log(age.value)
+    // Calcolo Prezzo Scontato
+    let salePrice= prezzoTotale - sale;
+
     document.querySelector(".details-name").innerHTML += "<span>" + name.value + "</span>";
-    document.querySelector("carriage").innerHTML += "<span>" + randomNumber + "</span>";
+    document.querySelector(".carriage").innerHTML += "<span>" + randomNumber + "</span>";
+    document.querySelector(".code-CP").innerHTML += "<span>" + codeCp + "</span>";
+    document.querySelector(".price-ticket").innerHTML += "<span>" + salePrice.toFixed(2) + "€" + "</span>";
+    
+  
+    
   }
 )
 
